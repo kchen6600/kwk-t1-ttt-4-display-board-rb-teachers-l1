df253847bf916a8fc433b67112ee639ee65475ee
Karen Chen <kchen6600@gmail.com>
2018-07-15 00:43:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} " + "\n" + "-----------" + "\n" + " #{board[3]} | #{board[4]} | #{board[5]} " + "\n" + "-----------"
-  print " #{board[6]} | #{board[7]} | #{board[8]}"
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
 end
