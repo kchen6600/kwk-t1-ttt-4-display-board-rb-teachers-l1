a7554ddd02dad89b21bc6184891c7532e18337f0
Karen Chen <kchen6600@gmail.com>
2018-07-15 00:42:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,3 +3,4 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} " + "\n" + "-----------" + "\n" + " #{board[3]} | #{board[4]} | #{board[5]} " + "\n" + "-----------"
   print " #{board[6]} | #{board[7]} | #{board[8]}"
+end
